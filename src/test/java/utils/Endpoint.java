da2c6b8bf4807f1e25828d4cd31d7a85296fc558
difaraka <rakaditya@alterra.id>
2019-09-14 15:36:57 +0700

    menambahkan fitur pembelian pulsa Sign in (pembayaran menggunakan VA)

--- src/test/java/utils/Endpoint.java
+++ src/test/java/utils/Endpoint.java
@@ -4,4 +4,9 @@ public interface Endpoint {
     String base = "https://altaqeproject.sepulsa.id/bumi";
 
     String login = base + "/account/login";
+    String guest_token = base + "/get_token";
+    String add_cart = base + "/bumi_cart/add";
+    String update_cart = base + "/bumi_checkout/cart_update";
+    String process_cart = base + "/bumi_checkout/process";
+    String complete_cart = base + "/bumi_checkout/complete";
 }
